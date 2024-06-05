2d99d073e35c30dcf3039ff38d658e9b55930203
Razibul Islam <dev.ronib@gmail.com>
2024-06-05 23:11:30 +0600

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,12 @@ import PublicLayout from "../layouts/PublicLayout.jsx";
 import Home from "../pages/Public/Home/Home.jsx";
 import Login from "../pages/Public/Auth/Login.jsx";
 import Register from "../pages/Public/Auth/Register.jsx";
+import Session from "../pages/Public/SessionDetails/Session.jsx";
+import SessionList from "../pages/Public/SessionList/SessionList.jsx";
+import PrivateLayout from "../layouts/PrivateLayout.jsx";
+import PrivateHome from "../pages/Private/PrivateHome/PrivateHome.jsx";
+import AdminHome from "../pages/Admin/AdminHome/AdminHome.jsx";
+import AdminLayout from "../layouts/AdminLayout.jsx";
 
 const router = createBrowserRouter([
     {
@@ -20,17 +26,51 @@ const router = createBrowserRouter([
                         path: '/',
                         element: <Home/>
                     },
-                    // Auth Routes
                     {
-                        path: '/login',
-                        element: <Login/>
+                        path: '/sessions',
+                        element: <SessionList/>
                     },
                     {
-                        path: '/register',
-                        element: <Register/>
+                        path: '/session/:id',
+                        element: <Session/>,
+                        loader: ({params}) => fetch(`http://localhost:5000/api/sessions/${params.id}`, {})
                     }
                 ]
             },
+
+            // Private Routes
+            {
+                path: '/dashboard',
+                element: <PrivateLayout/>,
+                children: [
+                    {
+                        path: '/dashboard',
+                        element: <PrivateHome/>,
+                    }
+                ]
+            },
+
+            // Admin Routes
+            {
+                path: '/admin',
+                element: <AdminLayout/>,
+                children: [
+                    {
+                        path: '/admin',
+                        element: <AdminHome/>
+                    }
+                ]
+            },
+
+            // Auth Routes
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/register',
+                element: <Register/>
+            }
         ]
     },
 ]);
