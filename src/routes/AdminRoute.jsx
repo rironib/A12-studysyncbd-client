c36e75e1fbfd3379fad689d5b75ece13be28f725
Razibul Islam <dev.ronib@gmail.com>
2024-06-05 23:10:49 +0600

    Create AdminRoute.jsx

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,27 @@
+import useAuth from "../hooks/useAuth.jsx";
+import useAdmin from "../hooks/useAdmin.jsx";
+import {Navigate, useLocation} from "react-router-dom";
+import PropTypes from "prop-types";
+import Loading from "../components/Loading.jsx";
+
+const AdminRoute = ({children}) => {
+    const {user, loading} = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
+        return <Loading/>;
+    }
+
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to='/dashboard' state={{from: location}} replace />
+};
+
+AdminRoute.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default AdminRoute;
