3f438c3f934d5b05b9143349a5b3ae44b9c8da6f
Razibul Islam <dev.ronib@gmail.com>
2024-06-05 23:11:03 +0600

    Update Header.jsx

--- src/components/Public/Header.jsx
+++ src/components/Public/Header.jsx
@@ -1,12 +1,24 @@
-import {Button, Heading, IconButton, Menu, MenuButton, MenuItem, MenuList, Stack} from "@chakra-ui/react";
+import {Avatar, Button, Heading, IconButton, Menu, MenuButton, MenuItem, MenuList, Stack} from "@chakra-ui/react";
 import {AddIcon, EditIcon, ExternalLinkIcon, HamburgerIcon, RepeatIcon} from "@chakra-ui/icons";
 import {Link, useNavigate} from "react-router-dom";
+import useAuth from "../../hooks/useAuth.jsx";
+import {toast} from "react-toastify";
 
 const Header = () => {
+    const {user, logOut} = useAuth();
     const navigate = useNavigate();
 
+    const handleLogout = () => {
+        logOut()
+            .then(() => {
+                toast.success("Logout successfully");
+                navigate('/login');
+            })
+            .catch((err) => toast.error(err));
+    }
+
     return (
-        <header className='w-full bg-slate-100 bg-opacity-50 text-slate-900 fixed z-10 shadow'>
+        <header className='w-full bg-slate-100 text-slate-900 fixed z-10 shadow'>
             <div className='w-11/12 lg:w-10/12 max-w-[1275px] mx-auto py-4'>
                 <div className='flex justify-between items-center'>
                     <div className='flex items-center gap-4'>
@@ -41,14 +53,28 @@ const Header = () => {
                     <div>
 
                     </div>
-                    <Stack direction='row' spacing={4} align='center'>
-                        <Button onClick={() => navigate('/login')} colorScheme='teal' variant='solid'>
-                            Login
-                        </Button>
-                        <Button onClick={() => navigate('/register')} colorScheme='teal' variant='outline'>
-                            Sign up
-                        </Button>
-                    </Stack>
+                    {
+                        user ? (
+                            <Stack direction='row' spacing={4} align='center'>
+                                <Avatar size='md' name={user.displayName} src={user.photoURL} />
+                                <Button onClick={() => navigate('/dashboard')} colorScheme='teal' variant='solid'>
+                                    Dashboard
+                                </Button>
+                                <Button onClick={handleLogout} colorScheme='teal' variant='outline'>
+                                    Logout
+                                </Button>
+                            </Stack>
+                        ) : (
+                            <Stack direction='row' spacing={4} align='center'>
+                                <Button onClick={() => navigate('/login')} colorScheme='teal' variant='solid'>
+                                    Login
+                                </Button>
+                                <Button onClick={() => navigate('/register')} colorScheme='teal' variant='outline'>
+                                    Sign up
+                                </Button>
+                            </Stack>
+                        )
+                    }
                 </div>
             </div>
         </header>
