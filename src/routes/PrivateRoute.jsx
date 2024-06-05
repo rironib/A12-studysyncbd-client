7ce76fae09aaf879c3f64065d0850ae92f1aaacf
Razibul Islam <dev.ronib@gmail.com>
2024-06-05 23:10:52 +0600

    Create PrivateRoute.jsx

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,31 @@
+import {Navigate, useLocation} from "react-router-dom";
+import PropTypes from "prop-types";
+import useAuth from "../hooks/useAuth.jsx";
+import useAdmin from "../hooks/useAdmin.jsx";
+import Loading from "../components/Loading.jsx";
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useAuth();
+    const location = useLocation();
+    const [isAdmin] = useAdmin();
+
+    if (loading) {
+        return <Loading/>;
+    }
+
+    if (user && !isAdmin) {
+        return children;
+    }
+
+    if (isAdmin) {
+        return <Navigate to='/admin' state={{from: location}} replace />
+    }
+
+    return <Navigate to="/login" state={{from: location}} replace />;
+};
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default PrivateRoute;
