229486cfd802af3c122c26206825051bf17f3a92
Razibul Islam <dev.ronib@gmail.com>
2024-06-06 14:52:50 +0600

    Admin Dashboard added

---
+++ src/components/Admin/Header.jsx
@@ -0,0 +1,81 @@
+import {Heading, IconButton, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {
+    AddIcon,
+    AtSignIcon,
+    AttachmentIcon, CalendarIcon, ChatIcon,
+    EditIcon,
+    HamburgerIcon,
+} from "@chakra-ui/icons";
+import {Link, NavLink, useNavigate} from "react-router-dom";
+import useAuth from "../../hooks/useAuth.jsx";
+import {toast} from "react-toastify";
+
+const Header = () => {
+    const {user, logOut} = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logOut()
+            .then(() => {
+                toast.success("Logout successfully");
+                navigate('/login');
+            })
+            .catch((err) => toast.error(err));
+    }
+
+    return (
+        <header className='w-full bg-slate-100 text-slate-900 sticky top-0 z-10 shadow'>
+            <div className='w-11/12 lg:w-10/12 max-w-[1275px] mx-auto py-3'>
+                <div className='flex justify-between items-center'>
+                    <div className='flex items-center gap-4'>
+                        <div className='lg:hidden'>
+                            <Menu>
+                                <MenuButton
+                                    as={IconButton}
+                                    aria-label='Options'
+                                    icon={<HamburgerIcon />}
+                                    variant='outline'
+                                />
+                                <MenuList>
+                                    <MenuItem onClick={() => navigate('/tutor/home')} icon={<AtSignIcon />}>
+                                        Dashboard
+                                    </MenuItem>
+                                    <MenuItem onClick={() => navigate('/tutor/create-sessions')} icon={<CalendarIcon />}>
+                                        Create Sessions
+                                    </MenuItem>
+                                    <MenuItem onClick={() => navigate('/tutor/sessions')} icon={<AddIcon />}>
+                                        Sessions
+                                    </MenuItem>
+                                    <MenuItem onClick={() => navigate('/tutor/add-materials')} icon={<EditIcon />}>
+                                        Add Materials
+                                    </MenuItem>
+                                    <MenuItem onClick={() => navigate('/tutor/materials')} icon={<AttachmentIcon />}>
+                                        Materials
+                                    </MenuItem>
+                                    <MenuItem onClick={() => navigate('/tutor/notes')} icon={<ChatIcon />}>
+                                        Notes
+                                    </MenuItem>
+                                </MenuList>
+                            </Menu>
+                        </div>
+                        <Link to='/'>
+                            <Heading as='h1' size='xl'>StudySync</Heading>
+                        </Link>
+                    </div>
+                    <div className='hidden lg:flex gap-4 font-semibold'>
+                        <NavLink to='/admin/home'>Dashboard</NavLink>
+                        <NavLink to='/admin/users'>Users</NavLink>
+                        <NavLink to='/admin/sessions'>Sessions</NavLink>
+                        <NavLink to='/admin/materials'>Materials</NavLink>
+                    </div>
+                    <div className='flex items-center gap-4'>
+                        <img src={user?.photoURL} alt={user?.displayName} className='w-10 lg:w-12 h-10 lg:h-12 rounded-full' />
+                        <button onClick={handleLogout} className='hidden lg:block bg-teal-700 text-white px-4 py-2 font-medium rounded'>Logout</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,22 +1,24 @@
 import {createBrowserRouter} from "react-router-dom";
 import RootLayout from "../layouts/RootLayout.jsx";
 import Error from "../pages/Public/Error/Error.jsx";
+
+// Public Routes
 import PublicLayout from "../layouts/PublicLayout.jsx";
 import Home from "../pages/Public/Home/Home.jsx";
 import Login from "../pages/Public/Auth/Login.jsx";
 import Register from "../pages/Public/Auth/Register.jsx";
 import Session from "../pages/Public/SessionDetails/Session.jsx";
 import SessionList from "../pages/Public/SessionList/SessionList.jsx";
-import PrivateLayout from "../layouts/PrivateLayout.jsx";
-import PrivateHome from "../pages/Private/PrivateHome/PrivateHome.jsx";
-import AdminHome from "../pages/Admin/AdminHome/AdminHome.jsx";
-import AdminLayout from "../layouts/AdminLayout.jsx";
+
+// Student Dashboard
 import StudentLayout from "../layouts/StudentLayout.jsx";
 import StudentHome from "../pages/Private/Student/StudentHome/StudentHome.jsx";
 import StudentSessions from "../pages/Private/Student/StudentSessions/StudentSessions.jsx";
 import StudentCreateNote from "../pages/Private/Student/StudentCreateNote/StudentCreateNote.jsx";
 import StudentManageNote from "../pages/Private/Student/StudentManageNote/StudentManageNote.jsx";
 import StudentMaterials from "../pages/Private/Student/StudentMaterials/StudentMaterials.jsx";
+
+// Tutor Dashboard
 import TutorLayout from "../layouts/TutorLayout.jsx";
 import TutorHome from "../pages/Tutor/Home/Home.jsx";
 import TutorCreateSessions from "../pages/Tutor/CreateSessions/TutorSessions.jsx";
@@ -24,6 +26,14 @@ import TutorSessions from "../pages/Tutor/Sessions/Sessions.jsx";
 import TutorAddMaterials from "../pages/Tutor/AddMaterials/AddMaterials.jsx";
 import TutorMaterials from "../pages/Tutor/Materials/Materials.jsx";
 import TutorNotes from "../pages/Tutor/Notes/Notes.jsx";
+
+// Admin Dashboard
+import AdminLayout from "../layouts/AdminLayout.jsx";
+import AdminHome from "../pages/Admin/Home/Home.jsx";
+import AdminUsers from "../pages/Admin/Users/Users.jsx";
+import AdminSessions from "../pages/Admin/Sessions/Sessions.jsx";
+import AdminMaterials from "../pages/Admin/Materials/Materials.jsx";
+
 
 const router = createBrowserRouter([
     {
@@ -117,9 +127,21 @@ const router = createBrowserRouter([
                 element: <AdminLayout/>,
                 children: [
                     {
-                        path: '/admin',
+                        path: '/admin/home',
                         element: <AdminHome/>
-                    }
+                    },
+                    {
+                        path: '/admin/users',
+                        element: <AdminUsers/>
+                    },
+                    {
+                        path: '/admin/sessions',
+                        element: <AdminSessions/>
+                    },
+                    {
+                        path: '/admin/materials',
+                        element: <AdminMaterials/>
+                    },
                 ]
             },
 
