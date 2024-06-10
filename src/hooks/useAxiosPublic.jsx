329bf2ac4db11707f818b20799a67e7860fb60b4
Razibul Islam <dev.ronib@gmail.com>
2024-06-10 23:45:01 +0600

    Update useAxiosPublic.jsx

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://studysyncbd.vercel.app',
 })
 
 const useAxiosPublic = () => {
