eb873c977088d73db6da9b1e62f9b2414c53d783
Razibul Islam <dev.ronib@gmail.com>
2024-06-10 23:45:18 +0600

    Update SessionList.jsx

--- src/pages/Public/SessionList/SessionList.jsx
+++ src/pages/Public/SessionList/SessionList.jsx
@@ -7,21 +7,16 @@ import {RiArticleLine, RiTimeLine, RiUser3Line} from "react-icons/ri";
 import {Link} from "react-router-dom";
 import {Helmet} from "react-helmet-async";
 import Loading from "../../../components/Shared/Loading.jsx";
+import Card from "../../../components/Shared/Card.jsx";
+import useSessions from "../../../hooks/useSessions.jsx";
 
 const SessionList = () => {
-    const {data: sessions = [], isLoading, error} = useQuery({
-        queryKey: ['sessions'],
-        queryFn: async () => {
-            const res = await axiosPublic.get('/api/sessions');
-            return res.data;
-        }
-    });
+    const [sessions, isLoading, , error] = useSessions();
+    const filteredSessions = sessions.filter((session) => session.status === "approved");
 
     if (isLoading) {
         return <Loading/>;
-    }
-
-    if (error) {
+    } else if (error) {
         return <ErrorAlert error={error}/>;
     }
 
@@ -36,47 +31,8 @@ const SessionList = () => {
                         <Text fontSize='xl' color='blue'>Our Sessions List</Text>
                         <Heading className='font-lexend'>All Sessions</Heading>
                     </Stack>
-                    <div>
-                        <Grid className="grid-cols-3 gap-8">
-                            {
-                                sessions.map((session) => (
-                                    <Box key={session._id} className='shadow rounded-lg'>
-                                        <img alt='cover' src={session.image}
-                                             className='rounded-xl'/>
-                                        <div className='mt-2'>
-                                            <div className='p-4 space-y-4'>
-                                                <Flex className='items-center gap-2'>
-                                                    <Rating style={{maxWidth: 110}} value={session.rating} readOnly/>
-                                                    <span>({session.rating})</span>
-                                                </Flex>
-                                                <div className='font-bold text-2xl'>
-                                                    {session.title}
-                                                </div>
-                                                <Flex spacing={4} direction='row' align='center'
-                                                      className='gap-4 *:flex *:items-center *:gap-2'>
-                                                    <div><RiUser3Line/> 0 Students</div>
-                                                    <div><RiArticleLine/> 15 Lessons</div>
-                                                    <div><RiTimeLine/> {session.sessionDuration}</div>
-                                                </Flex>
-                                            </div>
-                                            <Divider/>
-                                            <Flex className='justify-between p-4 py-2'>
-                                                <Flex className='items-center gap-2'>
-                                                    <WrapItem>
-                                                        <Avatar size='sm' name='Dan Abrahmov'
-                                                                src='https://bit.ly/dan-abramov'/>
-                                                    </WrapItem>
-                                                    <Text>{session.tutor}</Text>
-                                                </Flex>
-                                                <Link to={`/session/${session._id}`}>
-                                                    <Button>Read More</Button>
-                                                </Link>
-                                            </Flex>
-                                        </div>
-                                    </Box>
-                                ))
-                            }
-                        </Grid>
+                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+                        {filteredSessions.map((session) => <Card key={session._id} session={session}/>)}
                     </div>
                 </section>
             </main>
